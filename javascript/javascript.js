c8019d5ed4fcb98361e42cbbcc976484223dcee2
TikiCala <104035384+TikiCala@users.noreply.github.com>
2022-06-21 17:11:06 -0300

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,6 @@
+function modooscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function datos() {alert("Franscico Calahorra contacto: Franscisco_calahorra@hotmail.com")}
